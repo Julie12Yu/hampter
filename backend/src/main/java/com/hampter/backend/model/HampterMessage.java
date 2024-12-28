a9885b339f00ffcfcd91a815d5fc5ed43bef7de9
Julie Yu <julie.12.yu@gmail.com>
2024-12-27 22:06:13 -0600

    set up hampster message model (where users will attach message to hampster)

--- backend/src/main/java/com/hampter/backend/model/HampterMessage.java
+++ backend/src/main/java/com/hampter/backend/model/HampterMessage.java
@@ -1,5 +1,33 @@
 package com.hampter.backend.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "HampterMessage")
 public class HampterMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hampterMessageID")
+    private Integer hampterMessageID;
+
+    @Column(name = "fromUserID")
+    private Integer fromUserID;
+
+    @Column(name = "toUserID")
+    private Integer toUserID;
+
+    @Column(name = "messageTitle")
+    private String messageTitle;
+
+    @Column(name = "messageText")
+    private String messageText;
+
+    @Column(name = "sentTime")
+    private LocalDateTime sentDateTime;
+
+    @Column(name = "readDateTime")
+    private LocalDateTime readDateTime;
 
 }
