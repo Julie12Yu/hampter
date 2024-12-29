6c8b13cf7fa921c6ca9e9b5e6e0bd484397c6ed0
Julie Yu <julie.12.yu@gmail.com>
2024-12-28 21:31:59 -0600

    model for hampter complete

--- backend/src/main/java/com/hampter/backend/model/Hampter.java
+++ backend/src/main/java/com/hampter/backend/model/Hampter.java
@@ -1,5 +1,28 @@
 package com.hampter.backend.model;
 
-public class Hampter {
+import jakarta.persistence.*;
+import java.time.Duration;
 
+@Entity
+@Table(name = "hampter")
+public class Hampter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hampterID")
+    private Integer hampterID;
+
+    @Column(name = "hampterName")
+    private String hampterName;
+
+    @Column(name = "hampterModel")
+    private String hampterModel;
+
+    @Column(name = "hampterTime")
+    private Duration hampterTime;
+
+    @Column(name = "hampterHab")
+    private Integer hampterHab; // stores HAB ID
+
+    @Column(name = "hampterHoldMessageID")
+    private Integer hampterHoldMessageID;
 }
