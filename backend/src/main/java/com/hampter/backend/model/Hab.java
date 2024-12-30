cc230f808e3633f404a0e69c61b1bbb8e85e46ff
Julie Yu <julie.12.yu@gmail.com>
2024-12-30 12:28:36 -0600

    beginnign to set up graphql part

--- backend/src/main/java/com/hampter/backend/model/Hab.java
+++ backend/src/main/java/com/hampter/backend/model/Hab.java
@@ -1,5 +1,12 @@
 package com.hampter.backend.model;
 
-public class Hab {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "Hab")
+public class Hab {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "habID")
+    private Integer habID;
 }
