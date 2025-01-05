c917cb9ec2cea8c8b9abc0fda9d51ef6e75a6e27
Julie Yu <julie.12.yu@gmail.com>
2025-01-05 11:46:17 -0800

    prep for changing color modes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,10 +6,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,7 +38,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(), // allows dark mode - remove for light mode
+      themeMode: ThemeMode.system, // prefer system provided mode
       home: const MyHomePage(),
     );
   }
