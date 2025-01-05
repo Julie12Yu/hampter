1fd4ab057647f65fe48e00c89a6c1d23b1b6b90a
Julie Yu <julie.12.yu@gmail.com>
2025-01-05 12:44:15 -0800

    minor refactoring to make main cleaner

---
+++ frontend/lib/dark_mode_toggle.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class DarkModeSwitch extends StatefulWidget {
+  const DarkModeSwitch({super.key});
+
+  @override
+  State<DarkModeSwitch> createState() => _DarkModeSwitchState();
+}
+
+class _DarkModeSwitchState extends State<DarkModeSwitch> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      // This bool value toggles the switch.
+      value: light,
+      activeColor: Colors.deepPurpleAccent,
+      onChanged: (bool value) {
+        setState(() {
+          // called when the user toggles the switch.
+          if (value) {
+            // if switch is on, we're on dark mode
+            MyApp.of(context).changeMode(ThemeMode.dark);
+          } else {
+            MyApp.of(context).changeMode(ThemeMode.light);
+          }
+          light = value; // ensure state is up to date
+        });
+      },
+    );
+  }
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'testing_display.dart';
+import 'dark_mode_toggle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -73,38 +74,6 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class DarkModeSwitch extends StatefulWidget {
-  const DarkModeSwitch({super.key});
-
-  @override
-  State<DarkModeSwitch> createState() => _DarkModeSwitchState();
-}
-
-class _DarkModeSwitchState extends State<DarkModeSwitch> {
-  bool light = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Switch(
-      // This bool value toggles the switch.
-      value: light,
-      activeColor: Colors.deepPurpleAccent,
-      onChanged: (bool value) {
-        setState(() {
-          // called when the user toggles the switch.
-          if (value) {
-            // if switch is on, we're on dark mode
-            MyApp.of(context).changeMode(ThemeMode.dark);
-          } else {
-            MyApp.of(context).changeMode(ThemeMode.light);
-          }
-          light = value; // ensure state is up to date
-        });
-      },
-    );
-  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
