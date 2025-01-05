17506f5c5ac9f79a2b97ec413a8f38fbe1be1363
Julie Yu <julie.12.yu@gmail.com>
2025-01-05 11:47:06 -0800

    commnts detailing prep changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
+  // default is stateless - stateful to accept _MyAppState
   const MyApp({super.key});
 
   // This widget is the root of your application.
