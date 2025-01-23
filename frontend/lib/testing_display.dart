90a69dbcab82a5fda95b00a42cc6c7fd5f6fc727
Julie Yu <julie.12.yu@gmail.com>
2025-01-23 12:51:37 -0800

    code quality refactor

--- frontend/lib/testing_display.dart
+++ frontend/lib/testing_display.dart
@@ -14,44 +14,42 @@ class TestingDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FittedBox(
-        child: Row(
-          children: [
-            SizedBox(
-              width: 155.0,
-              height: 50.0,
-              child: FloatingActionButton(
-                heroTag: "Increment",
-                onPressed: onIncrement,
-                tooltip: 'PointIncrease',
-                child: const Icon(Icons.favorite),
-              ),
+    return FittedBox(
+      child: Row(
+        children: [
+          SizedBox(
+            width: 155.0,
+            height: 50.0,
+            child: FloatingActionButton(
+              heroTag: "Increment",
+              onPressed: onIncrement,
+              tooltip: 'PointIncrease',
+              child: const Icon(Icons.favorite),
             ),
-            const SizedBox(width: 10),
-            SizedBox(
-              width: 155.0,
-              height: 50.0,
-              child: FloatingActionButton(
-                heroTag: "HampterIncrease",
-                onPressed: onIncrementHampter,
-                tooltip: 'Hampster Increment',
-                child: const Icon(Icons.pets),
-              ),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: 155.0,
+            height: 50.0,
+            child: FloatingActionButton(
+              heroTag: "HampterIncrease",
+              onPressed: onIncrementHampter,
+              tooltip: 'Hampster Increment',
+              child: const Icon(Icons.pets),
             ),
-            const SizedBox(width: 10),
-            SizedBox(
-              width: 155.0,
-              height: 50.0,
-              child: FloatingActionButton(
-                heroTag: "Clear",
-                onPressed: onClearAll,
-                tooltip: 'Clear all',
-                child: const Icon(Icons.stop_circle),
-              ),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: 155.0,
+            height: 50.0,
+            child: FloatingActionButton(
+              heroTag: "Clear",
+              onPressed: onClearAll,
+              tooltip: 'Clear all',
+              child: const Icon(Icons.stop_circle),
+            ),
+          )
+        ],
       ),
     );
   }
