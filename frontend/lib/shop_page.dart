ae407c78b35bd48d799d226f401718da65f39bf6
Julie Yu <julie.12.yu@gmail.com>
2025-01-24 14:56:19 -0800

    Fixed titling for Shop

--- frontend/lib/shop_page.dart
+++ frontend/lib/shop_page.dart
@@ -15,7 +15,7 @@ class _ShopPageState extends State<ShopPage> {
         drawer: HamburgerMenu(),
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Settings Page"),
+          title: Text("Shop"),
         ),
         body: Text("hIIIIII"));
   }
