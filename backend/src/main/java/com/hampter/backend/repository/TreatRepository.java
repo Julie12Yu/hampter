6037d2ca412ac81c37dba1838cf913800a60114f
Julie Yu <julie.12.yu@gmail.com>
2025-01-29 15:36:47 -0800

    added repository (treat only still) to handle CRUD operations

---
+++ backend/src/main/java/com/hampter/backend/repository/TreatRepository.java
@@ -0,0 +1,10 @@
+package com.hampter.backend.repository;
+
+import com.hampter.backend.model.Treat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreatRepository extends JpaRepository<Treat, Integer> {
+
+}
