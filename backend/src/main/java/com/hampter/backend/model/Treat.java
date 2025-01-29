db7498091d5542db5b35a4996cffb27123cb605b
Julie Yu <julie.12.yu@gmail.com>
2025-01-29 15:32:48 -0800

    fixed treat, added lombok to treat (so i don't need to write getters and setters)

--- backend/src/main/java/com/hampter/backend/model/Treat.java
+++ backend/src/main/java/com/hampter/backend/model/Treat.java
@@ -1,13 +1,19 @@
 package com.hampter.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Treat")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Treat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "trinketID")
+    @Column(name = "treatID")
     private Integer Treat;
 
     @Column(name = "", columnDefinition = "string")
