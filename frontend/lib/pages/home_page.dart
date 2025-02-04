2cc4cb6af26c02fc6d68fb08ed65ab48668655e9
Julie Yu <julie.12.yu@gmail.com>
2025-02-04 10:40:32 -0800

    code quality change slight

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -77,9 +77,6 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       drawer: HamburgerMenu(),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the HomePage object that was created by
         // the App.build method, and use it to set our appbar title.
